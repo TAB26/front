0468fbd3a0f988eddb4683272a7b9beeab725083
Tamiru Belachew <87835386+TAB26@users.noreply.github.com>
2023-06-26 23:14:58 +0300

    Update EmployeeLogin.js

--- src/login/components/EmployeeLogin.js
+++ src/login/components/EmployeeLogin.js
@@ -22,7 +22,7 @@ const EmployeeLogin = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/employees/login", {
+      const response = await fetch("https://fleet-administration.onrender.com/api/employees/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
