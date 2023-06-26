3050ada83047abd4c044b63d043fe9cdc5a7bd1a
Tamiru Belachew <87835386+TAB26@users.noreply.github.com>
2023-06-26 23:13:48 +0300

    Update DeptHeadOfficerLogin.js

--- src/login/components/DeptHeadOfficerLogin.js
+++ src/login/components/DeptHeadOfficerLogin.js
@@ -22,7 +22,7 @@ const DeptHeadOfficerLogin = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch("http://localhost:5000/api/departmentHeads/login", {
+      const response = await fetch("https://fleet-administration.onrender.com//api/departmentHeads/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
