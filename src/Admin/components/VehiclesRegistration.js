2d843801389ead7c4f460538bb098433bd6a0c38
Tamiru Belachew <87835386+TAB26@users.noreply.github.com>
2023-06-26 23:40:31 +0300

    Update VehiclesRegistration.js

--- src/Admin/components/VehiclesRegistration.js
+++ src/Admin/components/VehiclesRegistration.js
@@ -38,7 +38,7 @@ vehicleStatus: "unassigned"
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/vehicles", {
+      const response = await fetch("https://fleet-administration.onrender.com/api/vehicles", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
