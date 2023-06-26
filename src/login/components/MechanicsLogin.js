7d938deb17bcd80efeec650b03a49a5aff5451ac
Tamiru Belachew <87835386+TAB26@users.noreply.github.com>
2023-06-26 23:16:06 +0300

    Update MechanicsLogin.js

--- src/login/components/MechanicsLogin.js
+++ src/login/components/MechanicsLogin.js
@@ -22,7 +22,7 @@ const MechanicsLogin = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/mechanics/login", {
+      const response = await fetch("https://fleet-administration.onrender.com/api/mechanics/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
