87ae783c32b5a2eec6c1f3180fda90a293161899
Tamiru Belachew <87835386+TAB26@users.noreply.github.com>
2023-06-26 21:29:35 +0300

    Update AdminLogin.js

--- src/login/components/AdminLogin.js
+++ src/login/components/AdminLogin.js
@@ -22,7 +22,7 @@ const AdminLogin = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/admins/login", {
+      const response = await fetch("https://fleet-backend-eta.vercel.app/api/admins/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
